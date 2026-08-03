cf743eb8714f2fdb40ab206451278273fde6baee
sidharthanm <53181481+sidharthanm@users.noreply.github.com>
2026-08-03 06:09:47 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,46 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        have = 0
+        need = len(set(t))
+        count = {}
+        final = {}
+        res =(1,10001)
+        for i in t:
+            final[i] = final.get(i,0) +1 
+        t = set(t)
+        l = -1
+        for i in range(len(s)):
+            if s[i] in t:
+                l = i
+                break
+        print(l)
+        if l == -1:
+            return ""        
+        r = l
+
+        while r<len(s):
+            if s[r] in t:
+                count[s[r]] = count.get(s[r],0) +1
+                if count[s[r]] == final[s[r]]:
+                    have+=1
+                
+            while have == need:
+                if (r-l) < res[1]-res[0]:
+                    res = (l,r)
+                
+                if s[l] in count:
+                    count[s[l]] -= 1
+                if s[l] in count and count[s[l]] < final[s[l]]:
+                    have -= 1
+                l+=1
+                while s[l] not in count:
+                    l+=1
+            r+=1
+        print(l)
+        l,r = res
+        print(l)
+        return s[l:r+1] if res[1] != 10001 else ""
+
+
+
+
